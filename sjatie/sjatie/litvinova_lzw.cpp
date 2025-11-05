aea9959fdbfe236ca568c4059a806b43b5ddc0fa
Litvik14 <ektltv@yandex.ru>
2025-11-05 16:53:37 +0300

    начало: создала файл litvinova_lzw.cpp

---
+++ sjatie/sjatie/litvinova_lzw.cpp
@@ -0,0 +1,13 @@
+#include "CompressionAlgorithms.h"
+
+using namespace std;
+
+CompressionResult litvinova_compress(const string& input) {
+    
+    return CompressionResult{};
+}
+
+string litvinova_decompress(const string& compressed) {
+    
+    return compressed;
+}
